a72557a3583ec8e1ff44503bf0c301425b2413a3
Daniel Farias <dan.tfarias@gmail.com>
2021-07-23 13:25:31 -0300

    Add files via upload

---
+++ functions02.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    print('~'*(len(msg)+4))
+    print('  {}'.format(msg))
+    print('~'*(len(msg)+4))
+
+
+escreva('Lorem ipsum id semper, tellus.')
+escreva('Lorem ipsum quisque neque tellus, facilisis tristique eget.')
+escreva('Dapibus etiam.')
+escreva('Leo odio integer phasellus, elementum.')
